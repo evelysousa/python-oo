e0103df6223f143f65066604a8dfa128c6a8bc39
Evely Sousa <evelysousa@gmail.com>
2023-07-04 20:16:27 -0300

    Aula 3 OO

---
+++ data.py
@@ -0,0 +1,7 @@
+class Datas:
+
+    def __init__(self, dia,mes, ano) -> None:
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+        print("A data é {} /{} / {}".format(self.dia,self.mes,self.ano))
